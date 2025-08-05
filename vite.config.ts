043cb307b419744c52bebc71d7b915a020d61197
marjamu2 <marjamu2@gmail.com>
2025-08-05 19:39:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+declare const process: any;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
