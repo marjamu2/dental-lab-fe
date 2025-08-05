728d180a6d3439489005b07a686b7d751174e394
marjamu2 <marjamu2@gmail.com>
2025-08-05 19:38:22 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Expone la variable de entorno al código del cliente.
+    // Vite reemplazará `process.env.API_KEY` con el valor real durante la compilación.
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  }
 })
